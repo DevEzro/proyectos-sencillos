0ceba83b0129e56a0f1678ae9d265136572abd1b
DevEzro <davidtestout@outlook.com>
2024-10-03 08:39:03 +0200

    Modificaciones + run.sh y run.bat modificados

--- avalonia/Prueba/run.bat
+++ avalonia/Prueba/run.bat
@@ -1,3 +1,54 @@
-dotnet clean
-dotnet build
-dotnet run
+@echo off
+setlocal enabledelayedexpansion
+
+:: Definir los colores (0A para verde, 0B para azul)
+set GREEN=0A
+set BLUE=0B
+
+:: Función para la animación
+:animate
+set message=%1
+set /a counter=0
+:loop
+set /a counter+=1
+if %counter% gtr 3 set /a counter=0
+
+if %counter%==0 (
+    echo [   ] %message%
+) else if %counter%==1 (
+    echo [.  ] %message%
+) else if %counter%==2 (
+    echo [.. ] %message%
+) else (
+    echo [...] %message%
+)
+timeout /t 1 /nobreak >nul
+goto loop
+
+:: Ejecutar comandos con animación
+
+:: Limpiar el proyecto
+start "" /b cmd /c call :animate "Limpiando el proyecto" & set anim_pid=!pid!
+dotnet clean >nul 2>&1
+taskkill /F /PID !anim_pid! >nul 2>&1
+color %GREEN%
+echo [✔] Limpiando el proyecto completado
+color
+
+:: Compilar el proyecto
+start "" /b cmd /c call :animate "Compilando el proyecto" & set anim_pid=!pid!
+dotnet build >nul 2>&1
+taskkill /F /PID !anim_pid! >nul 2>&1
+color %GREEN%
+echo [✔] Compilando el proyecto completado
+color
+
+:: Ejecutar el proyecto
+start "" /b cmd /c call :animate "Ejecutando el proyecto" & set anim_pid=!pid!
+dotnet run >nul 2>&1
+taskkill /F /PID !anim_pid! >nul 2>&1
+color %GREEN%
+echo [✔] Ejecutando el proyecto completado
+color
+
+endlocal
--- avalonia/Prueba/run.sh
+++ avalonia/Prueba/run.sh
@@ -1,3 +1,41 @@
-dotnet clean
-dotnet build
-dotnet run
+#!/bin/bash
+
+# Función para la animación
+animate() {
+    message=$1  # Mensaje a mostrar
+    
+    # Mientras se ejecuten los comandos se hará la animación de los puntos
+    # junto con el mensaje al lado
+    while true
+    do
+        echo -ne "\033[0;34m[   ] $message\r" # '\033[0:34m' es el color Azul en ANSI (para darle color a los mensajes)
+        sleep 0.33 # Espera 0.33 segundos entre animaciones
+        echo -ne "\033[0;34m[.  ] $message\r"
+        sleep 0.33
+        echo -ne "\033[0;34m[.. ] $message\r"
+        sleep 0.33
+        echo -ne "\033[0;34m[...] $message\r"
+        sleep 0.33
+    done
+}
+
+# Animación para el comando 'dotnet clean'
+animate "Limpiando el proyecto" &
+clean_pid=$!
+dotnet clean > /dev/null 2>&1
+kill $clean_pid
+echo -ne "\033[0;32m[✔] Limpiando el proyecto completado.\n" # '\033[0:34m' es el color Verde. Mismo caso que antes
+
+# Animación para el comando 'dotnet build'
+animate "Compilando el proyecto" &
+build_pid=$!
+dotnet build > /dev/null 2>&1
+kill $build_pid
+echo -ne "\033[0;32m[✔] Compilando el proyecto completado.\n"
+
+# Animación para el comando 'dotnet run'
+animate "Ejecutando el proyecto" &
+run_pid=$!
+dotnet run > /dev/null 2>&1
+kill $run_pid
+echo -ne "\033[0;32m[✔] Ejecutando el proyecto completado.\n"
